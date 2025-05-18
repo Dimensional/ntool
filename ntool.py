d4f814e64fd5d6311f274d5b045ceb269953aa78
Dimensional <5027297+Dimensional@users.noreply.github.com>
2025-05-18 18:06:42 -0500

    Update ntool.py
    Corrected help display

--- ntool.py
+++ ntool.py
@@ -2,7 +2,37 @@
 import sys
 from utils import *
 
-if sys.argv[1] in ['srl_retail2dev', 'cia_dev2retail', 'cia_retail2dev', 'cci_dev2retail', 'cci_retail2dev', 'csu2retailcias']:
+if len(sys.argv) <= 1 or sys.argv[1] == 'help':
+    print("To run this script, you need to provide arguments.")
+    print("Here is a list of arguments and examples for each one.")
+    print("")
+    print("Re-sign and re-encrypt NTR/TWL-enhanced/TWL-exclusive SRL for dev:")
+    print("python3 ntool.py srl_retail2dev <path_to_srl> (--out <path_to_output_file>)")
+    print("")
+    print("Re-sign and re-encrypt CIA/CCI for retail/dev:")
+    print("python3 ntool.py cia_dev2retail <path_to_cia> (--out <path_to_output_file>)")
+    print("python3 ntool.py cia_retail2dev <path_to_cia> (--out <path_to_output_file>)")
+    print("python3 ntool.py cci_dev2retail <path_to_cci> (--out <path_to_output_file>)")
+    print("python3 ntool.py cci_retail2dev <path_to_cci> (--out <path_to_output_file>)")
+    print("")
+    print("Convert CCI to CIA")
+    print("python3 ntool.py cci2cia <path_to_cci> (--out <path_to_output_file>) (--cci_dev) (--cia-dev)")
+    print("")
+    print("Convert CDN contents to CIA")
+    print("python3 ntool.py cdn2cia <path_to_cdn_folder> (--out <path_to_output_file>) (--title-ver <ver>) (--cdn-dev) (--cia-dev)")
+    print("")
+    print("Convert CIA to CDN contents")
+    print("python3 ntool.py cia2cdn <path_to_cia> (--out <path_to_output_folder>) (--titlekey <titlekey>) (--cia-dev)")
+    print("")
+    print("Full extraction and rebuild of NCCH/CIA/CCI:")
+    print("python3 ntool.py ncch_extractall <path_to_ncch> (--dev)")
+    print("python3 ntool.py ncch_rebuildall <path_to_folder> (--dev)")
+    print("python3 ntool.py cia_extractall <path_to_cia> (--dev)")
+    print("python3 ntool.py cia_rebuildall <path_to_folder> (--dev)")
+    print("python3 ntool.py cci_extractall <path_to_cci> (--dev)")
+    print("python3 ntool.py cci_rebuildall <path_to_folder> (--dev)")
+
+elif sys.argv[1] in ['srl_retail2dev', 'cia_dev2retail', 'cia_retail2dev', 'cci_dev2retail', 'cci_retail2dev', 'csu2retailcias']:
     path = sys.argv[2]
     out = ''
     for i in range(2, len(sys.argv)):
@@ -60,33 +90,3 @@ elif sys.argv[1] == 'cia2cdn':
         elif sys.argv[i] == '--cia-dev':
             cia_dev = 1
     cia2cdn(path, out, titlekey, cia_dev)
-
-elif sys.argv[1] == 'help' or sys.argv < 2:
-    print("To run this script, you need to provide arguments.")
-    print("Here is a list of arguments and examples for each one.")
-    print("")
-    print("Re-sign and re-encrypt NTR/TWL-enhanced/TWL-exclusive SRL for dev:")
-    print("python3 ntool.py srl_retail2dev <path_to_srl> (--out <path_to_output_file>)")
-    print("")
-    print("Re-sign and re-encrypt CIA/CCI for retail/dev:")
-    print("python3 ntool.py cia_dev2retail <path_to_cia> (--out <path_to_output_file>)")
-    print("python3 ntool.py cia_retail2dev <path_to_cia> (--out <path_to_output_file>)")
-    print("python3 ntool.py cci_dev2retail <path_to_cci> (--out <path_to_output_file>)")
-    print("python3 ntool.py cci_retail2dev <path_to_cci> (--out <path_to_output_file>)")
-    print("")
-    print("Convert CCI to CIA")
-    print("python3 ntool.py cci2cia <path_to_cci> (--out <path_to_output_file>) (--cci_dev) (--cia-dev)")
-    print("")
-    print("Convert CDN contents to CIA")
-    print("python3 ntool.py cdn2cia <path_to_cdn_folder> (--out <path_to_output_file>) (--title-ver <ver>) (--cdn-dev) (--cia-dev)")
-    print("")
-    print("Convert CIA to CDN contents")
-    print("python3 ntool.py cia2cdn <path_to_cia> (--out <path_to_output_folder>) (--titlekey <titlekey>) (--cia-dev)")
-    print("")
-    print("Full extraction and rebuild of NCCH/CIA/CCI:")
-    print("python3 ntool.py ncch_extractall <path_to_ncch> (--dev)")
-    print("python3 ntool.py ncch_rebuildall <path_to_folder> (--dev)")
-    print("python3 ntool.py cia_extractall <path_to_cia> (--dev)")
-    print("python3 ntool.py cia_rebuildall <path_to_folder> (--dev)")
-    print("python3 ntool.py cci_extractall <path_to_cci> (--dev)")
-    print("python3 ntool.py cci_rebuildall <path_to_folder> (--dev)")
